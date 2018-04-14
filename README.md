5bb20331811ab19e10915c2dab4b0379fe3af32e
Jeff Wang <jeff@joymode.com>
2018-04-14 04:38:48 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Week 4: Intro to JS Homework
+# Week 6: JS Functions and Arrays Homework
 
-There are four questions this week. You will need to declare variables, assign values, and combine the variables together in order to create a statement. 
+There are three questions this week. You will need to write functions and use arrays to come up with your solutions. 
 
 The index.html file contains the questions and directions. Open up the file in your web browser. You do not need to edit the code on this file.
-The exercises.js file is where you will be doing your homework. Declare variables, assign them values, and utilize the variables in order to create the answer statements.
+The exercises.js file is where you will be doing your homework. Declare variables, write functions, and call those functions in order to create the answer statements.
 
 If you are stuck, don't worry - Google is your best friend! If you still can't find what you are looking for, please reach out to us on Slack if you have any questions! 
 
--- exercises.js
+++ exercises.js
@@ -6,58 +6,14 @@
 // #1 The Fortune Teller
 //-----------------------------
 
-  // Since this is the first question, we will help you get started with two of the variables. Feel free to change the value to anything you want.
-  // You still need to add variables for the day of the week and the restaurant you will be going to.
 
-var numBurgers = 5;
-var restaurant = 'In n Out';
-
-  // We will also help you with part of the result, so that you know what it sort of looks like. REMEMBER, this is not the complete solution. 
-  // This is what the final output should look like: You will be eating _____ burgers at this restaurant: _____ with my friend, _____, on this day: ______
-
-var questionOneAnswer = 'You will be eating ' + numBurgers + ' burgers at this restaurant: ' + restaurant;
 
 
 
 
 //-----------------------------
-// #2 The Age Calculator 
+// #2 What Number Is Bigger? 
 //-----------------------------
-
-  //Forgot how old someone is? Calculate it! 
-  //You will have a total of 3 variables: the current year, the birth year, and a calculation based on the 2 years.
-  // Calculate their 2 possible ages based on the stored values. 
-
-
-// Store your answer here
-var questionTwoAnswer = '';
-
-
-
-
-//-----------------------------
-// #3 The Temperature Converter
-//-----------------------------
-
-
-// Celsius to Farenheit Answer
-var questionThreeAnswerA = '';
-
-// Farenheit to Celsius Answer
-var questionThreeAnswerB = '';
-
-
-
-
-//-----------------------------
-// #4 The Bonus Question
-//-----------------------------
-
-var questionFourAnswer = '';
-
-
-
-
 
 
 
@@ -65,15 +21,6 @@ var questionFourAnswer = '';
 
 
 //-----------------------------
-    //DO NOT EDIT THIS SECTION:
-    //This section will populate your HTML file with your answers.
+// #3 Top Choice
 //-----------------------------
-window.onload = function () {  
 
-  document.getElementById('answer1').innerHTML = questionOneAnswer;
-  document.getElementById('answer2').innerHTML = questionTwoAnswer;
-  document.getElementById('answer3a').innerHTML = questionThreeAnswerA;
-  document.getElementById('answer3b').innerHTML = questionThreeAnswerB;
-  document.getElementById('answer4').innerHTML = questionFourAnswer;
-
-}
